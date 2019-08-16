d6d1d04c7d15beedb8dc4b32136c10bf8b08f22b
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2019-08-16 15:47:18 +0600

    Add files via upload

---
+++ Custom Toast/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.practisefirebaseandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+ Button button1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //This layout infalter class will make the custom_layout a custom Toast
+                LayoutInflater inflater = getLayoutInflater();
+                //pass the xml name in the first parameter & the coresponding layout id in the second one(Must type cast in the second parameter)!
+               View customview = inflater.inflate(R.layout.custom_layout, (ViewGroup) findViewById(R.id.layoutId));
+
+                Toast toast = new Toast(MainActivity.this);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER,0,0);
+                toast.setView(customview);
+                toast.show();
+
+
+
+            }
+        });
+
+
+    }
+}
