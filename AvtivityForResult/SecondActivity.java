7bb919d39aabc93cc8f2f917f57c100971a41d1c
Plabon <46686524+PlabonKumarsaha@users.noreply.github.com>
2019-08-28 03:54:50 +0600

    Add files via upload

---
+++ AvtivityForResult/SecondActivity.java
@@ -0,0 +1,34 @@
+package com.example.activityforresult;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SecondActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        editText = findViewById(R.id.editText);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value = editText.getText().toString();
+                Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                intent.putExtra("key",value);
+                //you didnpt keep the result code same as the mainactivity one..so i assume they don't need to be same!!
+                setResult(0,intent);
+                finish();
+            }
+        });
+    }
+}
