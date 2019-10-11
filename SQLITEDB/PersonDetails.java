af7a51ab31dfba1d0b97ea1c7feb60b4492be2f4
Plabon <46686524+PlabonKumarsaha@users.noreply.github.com>
2019-10-12 00:31:56 +0600

    Add files via upload

---
+++ SQLITEDB/PersonDetails.java
@@ -0,0 +1,54 @@
+package Database;
+
+public class PersonDetails {
+
+    private String name;
+    private String email;
+    private String phone;
+    private String adress;
+    private String password;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+}
